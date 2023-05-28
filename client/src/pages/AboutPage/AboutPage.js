6d0186838eb7684434a577d4986e5b751a9a62b4
Faizan <faizanansari4862@gmail.com>
2023-05-29 01:27:06 +0530

    client About page added

---
+++ client/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AboutPage.css";
+
+const AboutPage = () => {
+  return (
+    <div className="about-container">
+      <h1>About Our Quiz App</h1>
+      <p>
+        Here, you can provide more information about the quiz app, its benefits,
+        and how it works.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
