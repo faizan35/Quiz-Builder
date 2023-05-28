9f0d8c7dfae6a4b4008fb3023f78e9a52498f729
Faizan <faizanansari4862@gmail.com>
2023-05-29 01:27:22 +0530

    client Contact page added

---
+++ client/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ContactPage.css";
+
+const ContactPage = () => {
+  return (
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <p>
+        Provide a contact form or details on how users can get in touch with
+        support or the institution.
+      </p>
+    </div>
+  );
+};
+
+export default ContactPage;
