fe1b9b9bcabb2e6ba225e452a39bebe15d918a6e
Faizan <faizanansari4862@gmail.com>
2023-05-29 01:25:45 +0530

    client assert and components added

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="quick-links">{/* Add your quick links here */}</div>
+      <div className="social-media">
+        {/* Add your social media icons here */}
+      </div>
+      <div className="contact-info">
+        {/* Add your contact information here */}
+      </div>
+      <div className="newsletter">
+        {/* Add your newsletter signup form here */}
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">Logo</a>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/how-to">How to</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+        <li>
+          <a href="/register">Register</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
