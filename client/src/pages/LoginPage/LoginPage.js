6d73bfb4c7b087d8f0423e6cf68b4bdcc9cd2293
Faizan <faizanansari4862@gmail.com>
2023-06-21 02:02:25 +0530

    client LoginPage Update

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../../redux/slices/authSlice";
 import "./LoginPage.css";
 
 const LoginPage = () => {
@@ -8,6 +11,8 @@ const LoginPage = () => {
     password: "",
   });
   const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -23,40 +28,59 @@ const LoginPage = () => {
       const response = await axios.post(
         "http://localhost:5000/api/users/login",
         formData
-      ); // Adjust the endpoint as per your backend setup
-      setMessage(response.data.message || "Successful Login"); // Assuming your backend sends a 'message' in the response
+      );
+
+      if (response.data && response.data.token) {
+        // Save token and user data to local storage
+        localStorage.setItem("token", response.data.token);
+        if (response.data.user) {
+          localStorage.setItem("user", JSON.stringify(response.data.user));
+        }
+
+        await dispatch(loginSuccess(response.data.user)); // Update the Redux store with user data
+        alert("Login Successful, Create Quiz.");
+        navigate("/"); // Redirect to home page
+      } else {
+        setMessage(response.data.message || "Successful Login");
+      }
     } catch (error) {
-      setMessage(error.response.data.error || "Login failed!"); // Handle error messages from backend or a generic message
+      setMessage(error.response.data.error || "Login failed!");
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      {message && <p className="message">{message}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+    <div className="login-page">
+      <div className="login-container">
+        <h2>Login</h2>
+        {message && <p className="message">{message}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div className="login-links">
+          <Link to="/register">Register</Link>
+          <Link to="/forgot-password">Forgot Password?</Link>
         </div>
-        <div className="input-group">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 };
