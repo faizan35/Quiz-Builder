7040a2a6994953215f847aab3ddedfe039260acd
Faizan <faizanansari4862@gmail.com>
2023-06-26 02:05:51 +0530

    client App.js Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
@@ -8,9 +8,38 @@ import HowToPage from "./pages/HowToPage/HowToPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import QuizPage from "./pages/QuizPage/QuizPage"; // <-- Import the QuizPage component
+import QuizPage from "./pages/QuizPage/QuizPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage/ForgotPasswordPage";
+import CreateQuizPage from "./pages/CreateQuizPage/CreateQuizPage";
+import AddEditQuestionPage from "./pages/AddEditQuestionPage/AddEditQuestionPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import UploadFilePage from "./pages/UploadFilePage/UploadFilePage";
 
 function App() {
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const tokenExpiry = localStorage.getItem("tokenExpiry");
+
+    if (token && tokenExpiry) {
+      const now = new Date();
+      const expiry = new Date(tokenExpiry);
+
+      if (now > expiry) {
+        // Token has expired
+        localStorage.removeItem("token");
+        localStorage.removeItem("tokenExpiry");
+        // Dispatch a logout action if you have one
+      } else {
+        // Extend the token expiry time
+        const newExpiryTime = new Date(
+          now.getTime() + 24 * 60 * 60 * 1000
+        ).toISOString();
+        localStorage.setItem("tokenExpiry", newExpiryTime);
+        // Dispatch a login action to update the Redux state if needed
+      }
+    }
+  }, []);
+
   return (
     <Router>
       <div>
@@ -22,9 +51,12 @@ function App() {
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/quiz" element={<QuizPage />} />{" "}
-          {/* <-- Add a route for the QuizPage */}
-          {/* Add more routes as needed */}
+          <Route path="/createquiz" element={<CreateQuizPage />} />
+          <Route path="/quiz-add-edit" element={<AddEditQuestionPage />} />
+          <Route path="/upload-file" element={<UploadFilePage />} />
+          <Route path="/quizpage" element={<QuizPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
         <Footer />
       </div>
